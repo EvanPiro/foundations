955e3f3dd0d1d43645a68d35ff78d4df958fd937
Alexandra Yanson <alexandrayanson@macbook-pro-7.home>
2020-04-09 20:44:54 -0400

    added to my page

--- pages/students/ally.tsx
+++ pages/students/ally.tsx
@@ -4,7 +4,8 @@ import * as React from 'react';
 const Ally = () => {
   return (
     <ContentPage title="Ally's Page">
-      This is your content page. Place any app code here to demo it for the world to see.
+      welcome 2 my page
+
     </ContentPage>
   )
 };
