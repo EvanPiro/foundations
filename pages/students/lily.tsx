3b3ad1cba7a03a12c5db88a7e8de3d01fd2a1f79
Lily Martin <lilykcmartin@gmail.com>
2020-04-09 20:55:40 -0400

    hi

--- pages/students/lily.tsx
+++ pages/students/lily.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 const Lily = () => {
   return (
     <ContentPage title="Lily's Page">
-      This is your content page. Place any app code here to demo it for the world to see.
+      ;P
     </ContentPage>
   )
 };
